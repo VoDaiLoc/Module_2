3945af5bad768a545e2055b37cd69e0549bb4863
VoDaiLoc <vodailoc.kt@gmail.com>
2022-05-16 13:09:05 +0700

    [Bài tập] Ứng dụng đọc số thành chữ

---
+++ read-number-app/src/Main.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Nhập vào số nguyên (0 <= number < 1000: ");
+            number = scanner.nextInt();
+        } while (number < 0 || number >= 1000);
+        int first = number / 100;
+        int second = (number % 100) / 10;
+        int third = number % 10;
+        if (number < 10) {
+            String result = zeroToNine(number);
+            System.out.println(result);
+            return;
+        }
+        if (number < 19) {
+            String result = _10_to_19(third);
+            System.out.println(result);
+            return;
+        }
+        if (number < 99) {
+            if (third != 0) {
+                String result = _20_to_99(second) + " " + zeroToNine(third);
+                System.out.println(result);
+                return;
+            }
+            String result = _20_to_99(second);
+            System.out.println(result);
+            return;
+        }
+        if (number < 1000) {
+            if (third != 0) {
+                String result = zeroToNine(first) + " hundred and " + _20_to_99(second) + " " + zeroToNine(third);
+                System.out.println(result);
+                return;
+            }
+            String result = zeroToNine(first) + " hundred and " + _20_to_99(second);
+            System.out.println(result);
+            return;
+        }
+    }
+
+    public static String zeroToNine(int number) {
+        String read = null;
+        switch (number) {
+            case 0:
+                read = "zero";
+                break;
+            case 1:
+                read = "one";
+                break;
+            case 2:
+                read = "two";
+                break;
+            case 3:
+                read = "three";
+                break;
+            case 4:
+                read = "four";
+                break;
+            case 5:
+                read = "five";
+                break;
+            case 6:
+                read = "six";
+                break;
+            case 7:
+                read = "seven";
+                break;
+            case 8:
+                read = "eight";
+                break;
+            case 9:
+                read = "nine";
+                break;
+        }
+        return read;
+    }
+
+    public static String _10_to_19(int number) {
+        String read = null;
+        switch (number) {
+            case 0:
+                read = "ten";
+                break;
+            case 1:
+                read = "eleven";
+                break;
+            case 2:
+                read = "twelwe";
+            case 3:
+                read = "thirteen";
+                break;
+            case 4:
+                read = "fourteen";
+                break;
+            case 5:
+                read = "fifteen";
+                break;
+            case 6:
+                read = "sixteen";
+                break;
+            case 7:
+                read = "seventeen";
+                break;
+            case 8:
+                read = "eighteen";
+                break;
+            case 9:
+                read = "nineteen";
+                break;
+        }
+        return read;
+    }
+
+    public static String _20_to_99(int number) {
+        String read = null;
+        switch (number) {
+            case 2:
+                read = "twenty";
+                break;
+            case 3:
+                read = "thirty";
+                break;
+            case 4:
+                read = "fourty";
+                break;
+            case 5:
+                read = "fifty";
+                break;
+            case 6:
+                read = "sixty";
+            case 7:
+                read = "seventy";
+                break;
+            case 8:
+                read = "eighty";
+                break;
+            case 9:
+                read = "ninety";
+                break;
+        }
+        return read;
+    }
+}
