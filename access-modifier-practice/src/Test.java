e08717a7678ea3f3a63c39c2a1e2c265361a179f
VoDaiLoc <vodailoc.kt@gmail.com>
2022-05-25 16:57:14 +0700

    [Bài tập] Xây dựng lớp chỉ ghi trong Java

---
+++ access-modifier-practice/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setName("Loc");
+        st.setClasses("C0322G1");
+        System.out.println(st.display());;
+    }
+}
