2c7fe966d004e534e6516a52429128c5e97c41be
VoDaiLoc <vodailoc.kt@gmail.com>
2022-06-01 09:49:10 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ point2D-point3D/src/Main.java
@@ -0,0 +1,10 @@
+import javafx.geometry.Point3D;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(1,2);
+        System.out.println(Arrays.toString(point2D.getXY()));
+    }
+}
