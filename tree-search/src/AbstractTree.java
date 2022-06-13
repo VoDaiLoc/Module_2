b885c2cb4007d3038993eae0309f4a5a577ba044
VoDaiLoc <vodailoc.kt@gmail.com>
2022-06-13 17:04:51 +0700

    Bai tap tree-search

---
+++ tree-search/src/AbstractTree.java
@@ -0,0 +1,7 @@
+
+
+public abstract class AbstractTree<E> implements collectionframwork.treesearch.Tree<E> {
+    public void postorder() {};
+    public void inorder() {};
+    public void preorder() {};
+}
